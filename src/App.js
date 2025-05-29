a3839f388bd0d83bc8af344c278148a5475b6c67
hyegook <chjin4520@naver.com>
2025-05-29 16:17:05 +0900

    커밋 메시지 테스트

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("I run only once");
+    console.log("I run only once.");
   }, []);
   useEffect(() => {
-    console.log("I run when 'keyword' changes");
+    console.log("I run when 'keyword' changes.");
   }, [keyword]);
   useEffect(() => {
-    console.log("I run when 'counter' changes");
+    console.log("I run when 'counter' changes.");
   }, [counter]);
   useEffect(() => {
-    console.log("I run when keyword & counter chage");
+    console.log("I run when keyword & counter chage.");
   }, [keyword, counter]);
   return (
     <div>
